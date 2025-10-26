3698160a4241c3dd924009e3ab370441511c4692
Diana Dukhovskaya <k.dianavv@gmail.com>
2025-10-26 19:07:26 +0000

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if(
+      typeof sampleActivity !== 'string'
+      || sampleActivity === null
+      || Number(sampleActivity) <= 0
+      || Number(sampleActivity) > MODERN_ACTIVITY
+    ) return false;
+  if(typeof sampleActivity === 'string' && Number.isNaN(Number(sampleActivity))) return false;
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = (Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+    return Math.ceil(t);
 }
 
 module.exports = {
