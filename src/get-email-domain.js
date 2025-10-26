d4ad728d45f54a2aecda3d11cc44013a1ef25d00
Diana Dukhovskaya <k.dianavv@gmail.com>
2025-10-26 11:48:46 +0000

    feat: solve getEmailDomain task

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,8 +10,15 @@ const { NotImplementedError } = require('../lib');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email) {
+ const emailToSplit = email.split("");
+ let indexAt = 0;
+ for (let i = 0; i < emailToSplit.length; i += 1) {
+   if (emailToSplit[i] === '@') {
+     indexAt = i;
+   }
+ }
+  return email.slice(indexAt + 1);
 }
 
 module.exports = {
