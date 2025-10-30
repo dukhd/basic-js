d6f43efb95606f1c4ecb7f5c29f46183addc456a
Diana Dukhovskaya <k.dianavv@gmail.com>
2025-10-30 12:18:39 +0000

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,62 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+  direction(result) {
+    if (!this.isDirect) {
+      return result.reverse().join('');
+    }
+    return result.join('');
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      return "Incorrect arguments!";
+    }
+    let messageArray = message.toUpperCase().split('');
+    let keyUppC = key.toUpperCase();
+    let keyIndex = 0;
+    let result = [];
+
+    for (let i = 0; i < messageArray.length; i += 1) {
+      if (messageArray[i] >= 'A' && messageArray[i] <= 'Z') {
+        let mCode = messageArray[i].charCodeAt(0) - 65;
+        let kCode = keyUppC[keyIndex % keyUppC.length].charCodeAt(0) - 65;
+        let newCode = (mCode + kCode) % 26;
+        result.push(String.fromCharCode(newCode + 65));
+        keyIndex += 1;
+      } else {
+        result.push(messageArray[i]);
+      }
+    }
+
+    return this.direction(result);
+  }
+
+  decrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      return "Incorrect arguments!";
+    }
+    let messageArray = message.toUpperCase().split('');
+    let keyUppC = key.toUpperCase();
+    let keyIndex = 0;
+    let result = [];
+
+    for (let i = 0; i < messageArray.length; i += 1) {
+      if (messageArray[i] >= 'A' && messageArray[i] <= 'Z') {
+        let mCode = messageArray[i].charCodeAt(0) - 65;
+        let kCode = keyUppC[keyIndex % keyUppC.length].charCodeAt(0) - 65;
+        let newCode = (mCode - kCode + 26) % 26;
+        result.push(String.fromCharCode(newCode + 65));
+        keyIndex += 1;
+      } else {
+        result.push(messageArray[i]);
+      }
+    }
+
+    return this.direction(result);
   }
 }
 
